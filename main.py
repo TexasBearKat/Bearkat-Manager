37792b7d6b0f4d2d9b90308531a231409e77786b
TexasBearKat <125840338+TexasBearKat@users.noreply.github.com>
2023-04-07 11:20:57 -0500

    Added more comments and help menu

--- main.py
+++ main.py
@@ -21,34 +21,54 @@ def add_site (site, email, password):
 # Checks if inputted password is set to autogenerate
     if password[-3:] == '---':
         rphrase = ''
+# Chooses 10 random integers between 1 and 100 and adds them to rphrase
         for i in range(1, 10):
-            rphrase += str(random.randint(1, 100)) # Chooses 10 random integers between 1 and 100 and adds them to rphrase
-        password = create_password(rphrase) # Sets password to random password
+            rphrase += str(random.randint(1, 100))
+# Sets password to random password using rphrase
+        password = create_password(rphrase) 
 # Don't touch, might explode       (no guarantees)
     with open("passwords.txt", "a") as f:
         f.write(f"\n{site} | {email} | {password}")
+# Prints help menu
+print("""
+as - Add site, asks for site name, email, and password
+cpw - Create Password, asks for password
+gpw - Get Password, gets password and email with site name
+q - Quit, quits
+help - Opens help menu
+        """)
 
 while True:
     c = input(">")
+# Create Password
     if c == "cpw":
         password = input("Enter phrase: ")
         print(create_password(password))
-    
+# Get Password
     elif c == "gpw":
         site = input("Site name: ")
         print(get_password(site))
-    
+# Add site
     elif c == "as":
         url = input("Site name: ")
         email = input("Email: ")
         passw = input("Password [--- for auto generate]: ")
         add_site(url, email, passw)
-        
+# Validates site was added
         if get_password(url) != None:
             print(f"Added site '{url}' successfully" )
         else:
             print("Error, site not added")
-
+# Help menu
+    elif c == "help":
+        print("""
+as - Add site, asks for site name, email, and password
+cpw - Create Password, asks for password
+gpw - Get Password, gets password and email with site name
+q - Quit, quits
+help - Opens help menu
+        """)
+# Quit
     elif c == "q":
         print("Goodbye")
         input("\n--| PRESS ENTER TO QUIT |--")
