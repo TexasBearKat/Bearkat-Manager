51b500e9aaaf46bbab4401b04f397924afd1bca8
TexasBearKat <125840338+TexasBearKat@users.noreply.github.com>
2023-04-07 11:03:29 -0500

    Updated main.py to actually make add site work
    So about calling the function...
    i forgor 💀
    It's fixed now
    official release coming soon (and by soon i mean never)

--- main.py
+++ main.py
@@ -1,42 +1,55 @@
 import hashlib
 import random
+# import father
 
+# Creates password using the SHA-256 algorithm
 def create_password(phrase):
     phrase = phrase.encode("utf-8")
     pw = hashlib.sha256()
     pw.update(phrase)
     return pw.hexdigest()
-
+# Gets the passwords of an inputted site
 def get_password(site):
+# Opens passwords.txt and looks through lines for site
+# Once it finds it it returns the line
     with open('passwords.txt') as pw_file:
         for line in pw_file:
             if site in line:
                 return line
-        
+# Adds a site to passwords.txt
 def add_site (site, email, password):
+# Checks if inputted password is set to autogenerate
     if password[-3:] == '---':
         rphrase = ''
         for i in range(1, 10):
-            rphrase += str(random.randint(1, 100))
-        password = create_password(rphrase)
-        
-    f = open("passwords.txt", "a")
-    f.write(site+" | "+email+" | "+password)
+            rphrase += str(random.randint(1, 100)) # Chooses 10 random integers between 1 and 100 and adds them to rphrase
+        password = create_password(rphrase) # Sets password to random password
+# Don't touch, might explode       (no guarantees)
+    with open("passwords.txt", "a") as f:
+        f.write(f"\n{site} | {email} | {password}")
 
 while True:
     c = input(">")
     if c == "cpw":
         password = input("Enter phrase: ")
         print(create_password(password))
+    
     elif c == "gpw":
-        url = input("Site name: ")
-        print(get_password(url))
-        url = 0
+        site = input("Site name: ")
+        print(get_password(site))
+    
     elif c == "as":
         url = input("Site name: ")
         email = input("Email: ")
         passw = input("Password [--- for auto generate]: ")
+        add_site(url, email, passw)
+        
+        if get_password(url) != None:
+            print(f"Added site '{url}' successfully" )
+        else:
+            print("Error, site not added")
+
     elif c == "q":
         print("Goodbye")
-        input("\n--| PRESS ANY KEY TO QUIT |--")
+        input("\n--| PRESS ENTER TO QUIT |--")
         break
